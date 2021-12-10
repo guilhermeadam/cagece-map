1ec3c603545ae1e140898c9ecd33c3f93d6a7966
guilhermeadam <guilhermeaddam@gmail.com>
2021-12-10 15:03:50 -0300

    testando arquivos .json

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import './styles/leaflet.css';
+import cageceJson from './database/cagece.json';
 import { MapContainer, TileLayer, Marker, Popup, Rectangle } from 'react-leaflet'
 
 
 function App() {
 
-  const cagece = [-3.771669,-38.5373133]
+  const cagece = [-3.771669, -38.5373133];
 
-
-  const blackOptions = { color: 'black' }
+  const blackOptions = { color: 'black' };
 
   const rectangle = [
     [51.49, -0.08],
     [51.5, -0.06],
-  ]
+  ];
 
   return (
     <div className="App">
+
       <MapContainer center={cagece} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={cagece}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
         </Marker>
-        <Rectangle 
-          bounds={rectangle} 
-          pathOptions={blackOptions}
-          
-        />
+        {cageceJson.map(json => <Marker position={[json.log,json.lat]}>
+          <Popup position={[json.log,json.lat]}>{[json.log,json.lat]}</Popup>
+        </Marker>)}
+
       </MapContainer>
     </div>
   );
