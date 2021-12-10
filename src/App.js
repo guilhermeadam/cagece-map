4151d26678348e098a9a7b1816e6504e7295dda2
guilhermeadam <guilhermeaddam@gmail.com>
2021-12-10 13:42:41 -0300

    instalando react-leaflet e suas dependencias

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import './styles/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 
 function App() {
   return (
     <div className="App">
-     <p>Projeto - Cagece Map</p>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
