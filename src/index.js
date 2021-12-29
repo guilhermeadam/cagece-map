85e5cb484755878bda429c239af1f696cbf39c15
guilhermeadam <guilhermeaddam@gmail.com>
2021-12-29 09:02:49 -0300

    commit apenas de backup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Container from './components/Container';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Container />
   </React.StrictMode>,
   document.getElementById('root')
 );
